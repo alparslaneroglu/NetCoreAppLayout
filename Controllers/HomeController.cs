12f999db203faead6677cb8a5c86216f97dc23c0
alparslaneroglu <furkanalparslaneroglu@gmail.com>
2022-01-13 13:10:59 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAppLayout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // Iactionresult ile actionresult aynı resultları döndürebilir fakaatt IActionResuş interface olarak işaretlenmiştri.Net core da 
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
